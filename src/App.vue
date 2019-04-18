8785cefe085ddaea654de25a82cc924999db8ef6
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-04-18 07:33:30 +0200

    basic implemantation 🔮🎉

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,93 @@
 <template>
-  <div>
-    <h1>{{title}}</h1>
-    {{runningTotal}}/{{totalBudget}}
-    <div @click="addToBasket(card)" v-for="card in cards" v-bind:key="card.id">{{ card.name }}</div>
-  </div>
+    <div class="wrapper">
+        <span class="chip mdl-chip mdl-chip--contact">
+            <span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">€</span>
+            <span class="mdl-chip__text">{{runningTotal}}/{{totalBudget}}</span>
+        </span>
+
+        <card v-if="!finish" v-on:addToB="addToBasket" :data="cards[cardIndex]"></card>
+        <div v-if="finish">
+            <button @click="reload" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                <i class="material-icons">autorenew</i>
+            </button>
+        </div>
+    </div>
 </template>
 
 <script>
-import { BUDGET } from './constants';
-const cards = require('../data/cards.json');
-export default {
-  name: 'App',
-  data() {
-    return {
-      title: 'Budget Spel 💶',
-      totalBudget: BUDGET,
-      cards,
-      basket: [],
+    import {
+        BUDGET
+    } from './constants';
+    const cards = require('../data/cards.json').sort(() => Math.random() - 0.5);;
+    import card from './Card';
+    export default {
+        name: 'App',
+        data() {
+            return {
+                title: 'Budget Spel 💶',
+                totalBudget: BUDGET,
+                cards,
+                basket: [],
+                cardIndex: 0,
+                finish: false,
+            };
+        },
+        mounted() {
+            // fetch('https://z0l80enr3f.execute-api.eu-west-1.amazonaws.com/latest/funds')
+            //   .then(x => x.json())
+            //   .then(funds => (this.funds = funds));
+        },
+        computed: {
+            runningTotal: function () {
+                return this.basket.reduce((a, b) => (a += b.price), 0);
+            },
+        },
+        components: {
+            card
+        },
+        methods: {
+            reload() {
+                window.location.reload(true);
+            },
+            addToBasket(item) {
+                if (!item) {
+                    this.navigateCards(1);
+                    return;
+                }
+                if (this.basket.find(basketItem => basketItem.id === item.id)) {
+                    item.bought = false;
+                    this.basket.splice(this.basket.indexOf(item), 1);
+                } else {
+                    item.bought = true;
+                    this.basket.push(item);
+                }
+                this.navigateCards(1);
+            },
+            navigateCards(amount) {
+                const newIndex = this.cardIndex + amount;
+                if (newIndex >= 0 && newIndex < this.cards.length) {
+                    this.cardIndex = newIndex;
+                }
+                if (newIndex === this.cards.length) {
+                    this.finish = true;
+                }
+            }
+        },
     };
-  },
-  mounted() {
-    // fetch('https://z0l80enr3f.execute-api.eu-west-1.amazonaws.com/latest/funds')
-    //   .then(x => x.json())
-    //   .then(funds => (this.funds = funds));
-  },
-  computed: {
-    runningTotal: function() {
-      return this.basket.reduce((a, b) => (a += b.price), 0);
-    },
-  },
-  methods: {
-    addToBasket(item) {
-      this.basket.push(item);
-    },
-  },
-};
 </script>
 
-<style>
+<style lang="less" scoped>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .bought {
+        text-decoration: line-through;
+    }
+
+    .chip {
+        margin: 4rem;
+    }
 </style>
---
+++ src/Card.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+
+        <!-- <h1>{{data.name}}</h1>
+        <button @click="$emit('addToB', data)"
+            class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+            <i class="material-icons">shopping_cart</i>
+        </button> -->
+
+        <!-- Square card -->
+
+        <div class="demo-card-square mdl-card mdl-shadow--2dp">
+            <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">{{data.name}}</h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+                {{data.priceString}}
+            </div>
+            <div class="mdl-card__actions mdl-card--border">
+                <a @click="$emit('addToB', data)"
+                    class="float-right mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    Kopen <i class="material-icons">shopping_cart</i> </a>
+                <a :disabled="data.obligatory" @click="!data.obligatory && $emit('addToB')"
+                    class="float-right mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                    Overslaan
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            data: {
+                type: Object,
+                required: true,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .demo-card-square.mdl-card {
+        width: 320px;
+        height: 320px;
+    }
+
+    .demo-card-square>.mdl-card__title {
+        color: #fff;
+        background:
+            #46B6AC;
+    }
+
+    .float-right {
+        float: right;
+    }
+</style>
